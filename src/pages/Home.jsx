c240f29d459e0b63d6326ff0fb6373c3744f4ff0
Sree Lakshmi Dileep <sreelakshmidileep2004@gmail.com>
2025-10-18 20:35:47 +0530

    Styles applied to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,20 @@ export default function Home() {
   const { user } = useAuth();
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1> Welcome to ReBooked</h1>
+    <div style={{ textAlign: "center", marginTop: "5em",fontSize:40 }}>
+      <h1 style={{color:"#1f3471ff"}}> Welcome to ReBooked</h1>
       <p>A platform for students to buy, sell, and exchange books easily.</p>
 
-      {!user ? (
-        <div>
-          <Link to="/login"><button>Login</button></Link>
-          <Link to="/register"><button>Register</button></Link>
-          <Link to="/about"><button>About</button></Link>
-        </div>
-      ) : (
-        <div>
-          <Link to="/dashboard"><button>Go to Dashboard</button></Link>
-        </div>
-      )}
+{user && (
+  <div>
+    <Link to="/dashboard">
+      <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+        Go to Dashboard
+      </button>
+    </Link>
+  </div>
+)}
+
     </div>
   );
 }
